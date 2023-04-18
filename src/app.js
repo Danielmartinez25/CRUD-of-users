e37ff30002eedbaa0770cc789622c9c2d0c0226c
Danimartinez25 <danimartinez1325@gmail.com>
2023-04-18 19:02:21 -0300

    init project crud

---
+++ src/app.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+let createError = require("http-errors");
+let express = require("express");
+let session = require("express-session");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
+const methodOverride = require("method-override");
+
+
+
+let usersRouterApi = require("./routes/api/users");
+
+
+let app = express();
+
+// view engine setup
+
+app.use(methodOverride("_method"));
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../public")));
+
+
+
+
+app.use("/api/users", usersRouterApi);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
