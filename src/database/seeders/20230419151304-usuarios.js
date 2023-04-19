9b7ce0cc53a7b231d803a5da1764335e7e9a4fa0
Danimartinez25 <danimartinez1325@gmail.com>
2023-04-19 12:27:57 -0300

    create table in database

---
+++ src/database/seeders/20230419151304-usuarios.js
@@ -0,0 +1,17 @@
+const { hashSync } = require("bcryptjs");
+const usersDB = require("../../data/users.json");
+const users = usersDB.map((user) => {
+  return {
+    ...user,
+    createdAt: new Date(),
+  };
+});
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("Users", users, {});
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
+
